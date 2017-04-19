4dbf7bc40cbacd32e19f7bf8dc902bf541d4e2cb
Yamen <osaretinosayamen@outlook.com>
2017-04-19 10:13:01 +0100

    Update convertToNaira.js

--- convertToNaira.js
+++ convertToNaira.js
@@ -1,18 +1,18 @@
 
-
+// converts dollar_value to naira using money.js library
 function conversion(dollar_value){
   var naira_value = fx.convert(dollar_value);
   return naira_value;
 }
 
-
+// replaces all inner html texts (dollar values) with naira values
 function convertToNaira(){
 document.body.innerHTML = document.body.innerHTML.replace(/\$([\d,]+(?:\.\d+)?)/g,
  function (string, c1) {
     //If there are commas, get rid of them and record they were there
     var comma = c1.indexOf(',') != -1;
     c1 = c1.replace(/,/g, '');
-    //Parse and double
+    //Parse and double and round off to nearest integer
     c1 = Math.round(parseFloat(c1));
     var value = '' + Math.round(conversion(c1));
     //Reinsert commas if they were there before
@@ -27,7 +27,8 @@ document.body.innerHTML = document.body.innerHTML.replace(/\$([\d,]+(?:\.\d+)?)/
 });
 }
 
-
+// recieves messages about button click from background.js
+// and then runs convertToNaira() function
 chrome.runtime.onMessage.addListener(
   function(request,sender,sendResponse){
     if(request.message === "clicked_browser_action"){
