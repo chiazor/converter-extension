e1bde248b9f052feaeb43eee7e7346f1dcde2db2
Yamen <osaretinosayamen@outlook.com>
2017-04-19 10:14:08 +0100

    Update getExchangeData.js

--- getExchangeData.js
+++ getExchangeData.js
@@ -1,3 +1,4 @@
+// gets exchange data from api using jquery
 $.getJSON(
     	// NB: using Open Exchange Rates here, but you can use any source!
         'https://openexchangerates.org/api/latest.json?app_id=0dd4b83391854ed2898d3f5308ea5848',
